696f331a22b519cf273129f4437d9847a5b6972b
Tony Gardella <tonygard@bu.edu>
2017-05-31 15:29:31 -0400

    fixes

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,19 @@ set -e
 [ "${TRAVIS_BRANCH}" != "master" ] && exit 0
 
 BOOK_DIR=$(pwd)/_book
+
 rm -rf ~/_book
+
 mkdir ~/_book && cd ~/_book
+
 git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git .
+
 ls | grep -v ^bookdown[.].* | xargs rm -rf
+
 git ls-files --deleted -z | xargs -0 git rm
+
 cp -r ${BOOK_DIR}/* ./
+
 git add --all *
 git commit -m"update book (travis build ${TRAVIS_BUILD_NUMBER})"
 git push -q -f origin gh-pages
