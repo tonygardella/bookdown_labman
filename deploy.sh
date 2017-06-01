f0c3f45effdd8665a4e69bc28486eb1f8c1dd3a5
Tony Gardella <tonygard@bu.edu>
2017-06-01 11:19:46 -0400

    deploy change

--- deploy.sh
+++ deploy.sh
@@ -11,12 +11,17 @@ git config --global user.name "Tony Gardella"
 
 
 BOOK_DIR=$(pwd)/_book
+
 rm -rf ~/_book
+
 mkdir ~/_book && cd ~/_book
+
 git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git .
-ls | grep -v ^bookdown[.].* | xargs rm -rf
-git ls-files --deleted -z | xargs -0 git rm
+
 cp -r ${BOOK_DIR}/* ./
+
 git add --all *
+
 git commit -m"update homepage (travis build ${TRAVIS_BUILD_NUMBER})"
+
 git push -q -f origin gh-pages
